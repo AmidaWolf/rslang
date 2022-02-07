13836b5c21f025bd75c17bd505d9f84fa6df63ce
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-02-07 19:06:29 +0300

    feat: add footer styles and elements, add .media

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import './global.scss';
 import './assets/fonts/fonts.css';
 import './components/view/pages/MainPage/MainPage.scss';
 import './components/shared/common/header/Header.scss';
+import './components/shared/common/footer/Footer.scss';
 import './components/shared/common/modal/Modal.scss';
 import './components/view/pages/TextbookPage/TextbookPage.scss';
 
