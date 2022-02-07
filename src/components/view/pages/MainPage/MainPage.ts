85f9173bbea7bd00bf3b99fb67a1551d93e36090
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-02-07 23:12:07 +0300

    feat: add features and benefits section

--- src/components/view/pages/MainPage/MainPage.ts
+++ src/components/view/pages/MainPage/MainPage.ts
@@ -18,7 +18,15 @@ export class MainPage implements Page {
   }
 
   async afterRender() {
-    removeLoading();
+    const header = <HTMLElement>document.querySelector('.header');
+    const footer = <HTMLElement>document.querySelector('.footer');
+    const main = <HTMLElement>document.querySelector('.main');
+
+    const headerHeight = header.getBoundingClientRect().height;
+    const footerHeight = footer.getBoundingClientRect().height;
+    main.style.minHeight = `calc(100vh - ${headerHeight + footerHeight}px)`;
+
+    await removeLoading();
   }
 
   async run() {
