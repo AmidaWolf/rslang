c5c88dd819c37a4e13107b39b7826f7c375f3888
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-02-07 17:42:28 +0300

    feat: add header styles and elements, add .media

--- src/components/shared/common/header/Header.ts
+++ src/components/shared/common/header/Header.ts
@@ -1,9 +1,21 @@
-// import { Page } from '../../../view/Page';
 import { RoutesPath, routesText } from '../../../app/routes';
 import baseHTML from './baseHTML';
+import { renderSources } from '../../helpers/renderSources';
 
 async function drawContent() {
   const headerWrapper = <HTMLElement>document.querySelector('.header-wrapper');
+
+  const logoWrapper = document.createElement('a');
+  logoWrapper.className = 'logo-wrapper';
+  logoWrapper.href = `/#${RoutesPath.START}`;
+  const logo = await renderSources.renderImage(
+    './../../../assets/logo.png',
+    'RS Lang logo'
+  );
+  logo.className = 'logo';
+  logo.width = 180;
+  logo.height = 50;
+  logoWrapper.appendChild(logo);
 
   const nav = document.createElement('nav');
   nav.className = 'nav';
@@ -12,19 +24,26 @@ async function drawContent() {
   menuList.className = 'header-menu';
   const pathList = Object.values(RoutesPath);
   for (let i = 0; i < pathList.length; i += 1) {
-    const menuItem = document.createElement('li');
-    menuItem.className = 'header-menu__item';
-    const menuRef = document.createElement('a');
-    menuRef.className = 'header-menu__link';
-    menuRef.href = `/#${pathList[i]}`;
-    menuRef.innerText = routesText[pathList[i]];
+    if (pathList[i] !== RoutesPath.AUTHORIZATION) {
+      const menuItem = document.createElement('li');
+      menuItem.className = 'header-menu__item';
+      const menuRef = document.createElement('a');
+      menuRef.className = 'header-menu__link';
+      menuRef.href = `/#${pathList[i]}`;
+      menuRef.innerText = routesText[pathList[i]];
 
-    menuItem.appendChild(menuRef);
-    menuList.appendChild(menuItem);
+      menuItem.appendChild(menuRef);
+      menuList.appendChild(menuItem);
+    }
   }
   nav.appendChild(menuList);
 
-  headerWrapper.appendChild(nav);
+  const login = document.createElement('a');
+  login.className = 'login button';
+  login.href = `/#${RoutesPath.AUTHORIZATION}`;
+  login.innerText = routesText[RoutesPath.AUTHORIZATION];
+
+  headerWrapper.append(logoWrapper, nav, login);
 }
 
 export class Header {
@@ -33,6 +52,6 @@ export class Header {
   }
 
   async afterRender() {
-    drawContent();
+    await drawContent();
   }
 }
